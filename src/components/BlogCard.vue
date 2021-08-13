63483647ef0c9e51b02a10966d30001c2ec0e479
Keith Kadima <keithkadima@gmail.com>
2021-08-13 13:07:26 +0300

    Add Blog Card Component | Complete

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,13 +1,134 @@
 <template>
-  <div></div>
+  <div class='blog-card'>
+    <div class="icons">
+      <div class="icon">
+        <Edit class='edit'/>
+      </div>
+      <div class="icon">
+        <Delete class='delete'/>
+      </div>
+    </div>    
+    <img :src="require(`../assets/blogCards/${post.blogCoverPhoto}.jpg`)" alt="blog photo" >
+    <div class="info">
+      <h4>{{ post.blogTitle }}</h4>
+      <h6>Posted on {{post.blogDate}} </h6>
+      <router-link class="link" to='#'>View the post <Arrow class='arrow'/></router-link>
+    </div>
+  </div>
 </template>
 
 <script>
+import Arrow from "../assets/Icons/arrow-right-light.svg"
+import Edit from "../assets/Icons/edit-regular.svg"
+import Delete from "../assets/Icons/trash-regular.svg"
 export default {
-
-}
+  name: 'blogCard',
+  props: ['post'],
+  components: {
+    Arrow,
+    Edit,
+    Delete,
+  },
+ };
 </script>
 
-<style>
+<style lang='scss' scoped>
+.blog-card{
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  background-color: #fff;
+  min-height: 420px;
+  transition: .5s ease all;
+  &:hover{
+    transform: rotateZ(3deg) scale(1.06);
+    box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6) ;
+  }
+
+  .icons{
+    display: flex;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    z-index: 99;
+    .icon{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 35px;
+      height: 35px;
+      border-radius: 50%;
+      background-color: #fff;
+      transition: .5s ease all;
+
+      &:hover{
+        background-color: #303030;
+
+        .edit,
+        .delete{
+          path{
+            fill: #fff;
+          }
+        }
+      }
+      &:nth-child(1){
+        margin-right: 8px;
+      }
+
+      .edit,
+      .delete{
+        pointer-events: none;
+        height: 15px;
+        width: auto;
+      }
+    }
+  }
+
+  img{
+    display: block;
+    z-index: 1;
+    border-radius: 8px 8px;
+    width: 100%;
+    min-height: 250px;
+    object-fit: cover;
+  }
+
+  .info{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    z-index: 1;
+    padding: 32px 16px;
+    color: black;
+    h4{
+      padding-bottom: 8px;
+      font-size: 20px;
+      font-weight: 300;
+    }
+    h6{
+      font-weight: 400;
+      font-size:16px;
+      padding-bottom: 16px;
+    }
+    .link{
+      display: inline-flex;
+      align-items: center;
+      margin-top: auto;
+      font-weight: 500;
+      padding-top: 20px;
+      font-size: 12px;
+      padding-bottom: 4px;
+      transition: 0.5s ease-in all;
+      &:hover{
+        color: rgba( 48, 48, 40, 0.8) 
+      }
+      .arrow{
+        width: 10px;
+      } 
+    }
+  }
+}
 
 </style>
