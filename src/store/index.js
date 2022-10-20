238a5b8f401566889540fa538a17262cbaa33d00
Akashik Overlord <keithkadima@gmail.com>
2022-10-20 09:40:53 +0000

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import db from '../firebase/firebaseInit';
 
 Vue.use(Vuex)
 
@@ -22,12 +25,36 @@ export default new Vuex.Store({
 
   },
   mutations: {
-    toggleEditPost(state, payload){
+    toggleEditPost(state, payload) {
       state.editPost = payload;
-      
-    }
+    },
+    
+    updateUser(state, payload){
+      state.user = payload; 
+    },
+    
+    setProfileInfo(state, doc) {
+      state.profileId = doc.id;
+      state.profileEmail = doc.data().email;
+      state.profileFirstName = doc.data().firstName;
+      state.profileLastName = doc.data().lastName;
+      state.profileUsername = doc.data().userName;
+    },
+    
+    setProfileInitials(state) {
+      state.profileInitials = 
+        state.profileFirstName.match(/(\b\S)?/g).join("") +
+        state.profileLastName.match(/(\b\S)?/g).join("");                            
+    },
+    
+    
   },
   actions: {
+    async getCurrentUser({ commit }){
+      const dataBase = await db.collection("users").doc(firebase.auth().currentUser.uid);
+      const dbResults = await dataBase.get();
+      commit("setProfileInfo", dbResults);
+      commit("setProfileInitials");
   },
   modules: {
   }
