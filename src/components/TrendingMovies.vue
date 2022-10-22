33c579e14d646cbd43ff8e202aecf46fa8094aaa
Akashik Overlord <keithkadima@gmail.com>
2022-10-22 11:47:14 +0000

    Create TrendingMovies.vue

---
+++ src/components/TrendingMovies.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul v-for="post in posts" v-bind:key="post.id">
+      <li>{{ post.title }}</li>
+      <p>{{ post.body }}</p>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("http://jsonplaceholder.typicode.com/posts");
+        this.posts = await response.json();;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
