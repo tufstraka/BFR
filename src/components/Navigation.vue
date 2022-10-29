0e0cb56e37996feb4b7f0288bb3e860a3293d613
kadimakeith <keithkadima@gmail.com>
2022-10-29 15:30:42 +0000

    implement profile-info module

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -13,8 +13,42 @@
                     <router-link class='link' to='#'>Create Review</router-link>
                     <router-link class='link' :to="{name:'Login'}">Login/Register</router-link>
                 </ul>
+                <div class="profile">
+                    <span>{{ this.$store.state.profileInitials }}</span>
+                    <div class="profile-menu">
+                        <div class="info">
+                            <p class="initials">{{ this.$store.state.profileInitials }}</p>
+                            <div class="right">
+                                <p>{{ this.$store.state.profileFirstName }} {{ this.$store.state.profileLastName }}</p>
+                                <p>{{ this.$store.state.profileUsername }}</p>
+                                <p>{{ this.$store.state.profileEmail }}</p>
+                            </div>
+                        </div>
+                        <div class="options">
+                            <div class="option">
+                                <router-link class="option" to="#">
+                                    <userIcon class="icon"/>
+                                    <p>Profile</p>
+                                </router-link>
+                            </div>
+                            <div class="option">
+                                <router-link class="option" to="#">
+                                    <adminIcon class="icon"/>
+                                    <p>Admin</p>
+                                </router-link>
+                            </div>
+                            <div class="option">
+                                <router-link class="option" to="#">
+                                    <signOutIcon class="icon"/>
+                                    <p>Sign Out</p>
+                                </router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
+
         <menuIcon @click='toggleMobileNav' class='menu-icon' v-show='mobile'/>
         <transition name='mobile-nav'>
             <ul class='mobile-nav' v-show='mobileNav'>
@@ -32,11 +66,17 @@
 
 <script>
     import menuIcon from '../assets/Icons/bars-regular.svg';
+    import userIcon from '../assets/Icons/user-alt-light.svg';
+    import adminIcon from '../assets/Icons/user-crown-light.svg';
+    import signOutIcon from '../assets/Icons/sign-out-alt-regular.svg';
 
     export default{
         name:'Navigation',
         components:{
-           menuIcon
+           menuIcon,
+           userIcon,
+           adminIcon,
+           signOutIcon
         },
         data() {
         return{
@@ -111,6 +151,85 @@ nav{
             }
             .link:last-child{
                 margin-right: 0;
+            }
+        }
+
+        .profile{
+            position: relative;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            color: #fff;
+            background-color: #303030;
+
+            .profile-menu{
+                position: absolute;
+                top: 60px;
+                right: 0;
+                width: 250px;
+                background-color: #303030;
+                box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6);
+
+                .info{
+                    display: flex;
+                    align-items: center;
+                    padding: 15px;
+                    border-bottom: 1px solid #fff;
+
+                    .initials{
+                        position: initial;
+                        width: 40px;
+                        height: 40px;
+                        background-color: #fff;
+                        color:#303030;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        border-radius: 50%;
+                    }
+
+                    .right{
+                        flex: 1;
+                        margin-left: 24px;
+
+                        p:nth-child(1) {
+                            font-size: 14px;
+                        }
+
+                        p:nth-child(2),
+                        p:nth-child(3) {
+                            font-size: 12px;
+                        }
+                    }
+                }
+
+                .options{
+                    padding: 15px;
+                    .option {
+                        text-decoration: none;
+                        color: #fff;
+                        display: flex;
+                        align-items: center;
+                        margin-bottom: 12px;
+
+                        .icon {
+                            width: 18px;
+                            height: auto;
+                        }
+                        p {
+                            font-size: 14px;
+                            margin-left: 12px;
+                        }
+
+                        &:last-child{
+                            margin-bottom: 0px;
+                        }
+                    }
+                }
             }
         }
     }
