306843ec1ec90d13090b281156037ecf2c3d5469
Akashik Overlord <keithkadima@gmail.com>
2022-10-29 06:14:01 +0000

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -55,6 +55,7 @@ export default new Vuex.Store({
       const dbResults = await dataBase.get();
       commit("setProfileInfo", dbResults);
       commit("setProfileInitials");},
+      console.log(dbResults);
   },
   modules: {
     
