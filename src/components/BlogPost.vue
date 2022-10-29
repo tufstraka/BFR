5050e9e56f6204611ccf16cd1cfc7cc3697f71af
kadimakeith <keithkadima@gmail.com>
2022-10-29 16:58:15 +0000

    add attribute value to v-bind(missing commas)

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class ='blog-wrapper' :class={'no-user' : !user}>
+  <div class ='blog-wrapper' :class="{'no-user' : !user}">
       <div class="blog-content">
           <div>
               <h2 v-if="post.welcomeScreen"> {{post.title}}</h2>
