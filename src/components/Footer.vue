00bedd315db7687e40c7e47311644443ac03322f
kadimakeith <keithkadima@gmail.com>
2022-10-31 06:48:31 +0000

    feat: add dynamic date in setup script

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,12 @@
+<script setup>
+
+    const date = document.getElementById("date");
+    const today = new Date()
+    const year = today.getFullYear();
+    date.innerHTML = year;
+    
+</script>
+
 <template>
     <footer>
         <div class="container">
@@ -43,11 +52,6 @@
     import twitter from '../assets/Icons/twitter-brands.svg';
     import instagram from '../assets/Icons/instagram-brands.svg';
     import linkedin from '../assets/Icons/linkedin-brands.svg';
-
-    const date = document.getElementById("date");
-    const today = new Date()
-    const year = today.getFullYear();
-    date.innerHTML = year;
 
     export default{
         name:'footer-vue',
