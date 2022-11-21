46d632195b524971d8a5712a23bbab59c1bc6440
kadimakeith <keithkadima@gmail.com>
2022-11-21 06:12:29 +0000

    fix: remove sample blog posts

--- src/store/index.js
+++ src/store/index.js
@@ -8,12 +8,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    sampleBlogCards:[
-      {blogTitle:'hdjkhsjfhgkjghf', blogCoverPhoto:'stock-1' , blogDate:'May 23, 2021'},
-      {blogTitle:'hdjkhsjfhgkjghf', blogCoverPhoto:'stock-2' , blogDate:'May 23, 2021'},
-      {blogTitle:'hdjkhsjfhgkjghf', blogCoverPhoto:'stock-3' , blogDate:'May 23, 2021'},
-      {blogTitle:'hdjkhsjfhgkjghf', blogCoverPhoto:'stock-4' , blogDate:'May 23, 2021'},
-    ],
     blogPosts: [],
     postLoaded: null,
     blogHTML: "Write your blog title here ...",
