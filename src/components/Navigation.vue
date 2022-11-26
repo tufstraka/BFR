88a450097aaf4409a5d17199d9813312389e7b6d
kadimakeith <keithkadima@gmail.com>
2022-11-26 07:26:25 +0000

    style: remove box-shadow

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -134,7 +134,7 @@
 header{
     background-color: #fff;
     padding: 0 25px;
-    box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6);
+    /*box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6)*/
     z-index: 99;
 }
 .link{
