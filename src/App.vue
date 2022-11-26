341ce24a62e63403c847c8b022f07730150dd0cf
kadimakeith <keithkadima@gmail.com>
2022-11-26 08:16:53 +0000

    style: change app font to poppins

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@ methods: {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
 * {
   margin: 0;
   padding: 0;
