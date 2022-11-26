6528f8f153bbb864cbb2f97e10a62b21cdd76c53
kadimakeith <keithkadima@gmail.com>
2022-11-26 08:21:27 +0000

    style: change app font to Barlow Semi Condensed

--- src/App.vue
+++ src/App.vue
@@ -64,12 +64,12 @@ methods: {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@100&display=swap');
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
+  font-family: "Barlow Semi Condensed", sans-serif;
 }
 
 
