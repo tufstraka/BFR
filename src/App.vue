1480ad35e952f2850f75c46deea773f576f0034b
kadimakeith <keithkadima@gmail.com>
2023-01-25 16:07:22 +0000

    style: scrollbar

--- src/App.vue
+++ src/App.vue
@@ -73,21 +73,21 @@ methods: {
 }
 
 
-body::-webkit-scrollbar{
+*::-webkit-scrollbar{
     width: 1.26rem;
 }
-body::-webkit-scrollbar-thumb {
+*::-webkit-scrollbar-thumb {
     background-color: #d6dee1;
     border-radius: 20px;
     border: 6px solid transparent;
     background-clip: content-box;
   }
 
-body::-webkit-scrollbar-track {
+*::-webkit-scrollbar-track {
     background-color: transparent;
   }
   
-body::-webkit-scrollbar-thumb:hover {
+*::-webkit-scrollbar-thumb:hover {
     background-color: rgb(233, 48, 48);
   }  
 
