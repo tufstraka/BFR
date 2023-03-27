849547e280d6091e133a9e3a7f3c30499455db4e
kadimakeith <keithkadima@gmail.com>
2023-03-27 09:16:19 +0000

    revert footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,90 +1,153 @@
 <template>
-    <footer>
-        <div class="container">
-                <div class="col-1">
-                    <router-link class="header" :to="{name:'home'} ">Biased Film Reviews</router-link>
-                    <ul>
-                        <li>
-                            <a href="#"><youTube class="svg-icon"/></a>
-                        </li>
-                        <li>
-                            <a href="#"><twitter class="svg-icon"/></a>
-                        </li>
-                        <li>
-                            <a href="#"><instagram class="svg-icon"/></a>
-                        </li>
-                        <li>
-                            <a href="#"><linkedin class="svg-icon"/></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <ul>
-                        <router-link class="link" :to="{ name: 'home'}" >Home</router-link>
-                        <router-link class="link" :to="{ name: 'reviews'}" >Reviews</router-link>
-                        <router-link class="link" :to="{ name: 'home'}" >News</router-link>
-                        <router-link class="link" :to="{ name: 'home'}" >Blog</router-link>
-                        <router-link class="link" :to="{ name: 'home'}" >Privacy policy</router-link>
-                        <router-link class="link" :to="{ name: 'home'}" >Cookie policy</router-link>
-                    </ul>
-                </div>
-                <div class="col-3">
-                    <p><span id="date">{{ this.addDate() }}</span> &#169; | All Rights Reserved</p> <br> 
-                </div>
-        </div>   
-       
-    </footer>
-     
-</template>
-
-<script>
-    import youTube from '../assets/Icons/youtube-brands.svg';
-    import twitter from '../assets/Icons/twitter-brands.svg';
-    import instagram from '../assets/Icons/instagram-brands.svg';
-    import linkedin from '../assets/Icons/linkedin-brands.svg';
-
-    export default{
-        name:'footer-vue',
-        components:{
-            youTube,
-            twitter,
-            instagram,
-            linkedin
-        },
-
-        methods: {
-            addDate(){
-
-            const today = new Date();
-            const year = today.getFullYear();
-            return year;
-
-            },
-        },
+    <div class='blog-card'>
+      <div v-show='editPost' class="icons">
+        <div @click="editBlog" class="icon">
+          <Edit class='edit'/>
+        </div>
+        <div @click="deletePost" class="icon">
+          <Delete class='delete'/>
+        </div>
+      </div>    
+      <img :src="post.blogCoverPhoto" alt="blog photo" >
+      <div class="info">
+        <h4>{{ post.blogTitle }}</h4>
+        <h6>Posted on {{ new Date(post.blogDate).toLocaleString("en-us", { dateStyle: "long"}) }} </h6>
+        <router-link class="link" :to="{name: 'ViewBlog', params: {blogid: this.post.blogID}}">
+          View the post <Arrow class='arrow'/></router-link>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Arrow from "../assets/Icons/arrow-right-light.svg"
+  import Edit from "../assets/Icons/edit-regular.svg"
+  import Delete from "../assets/Icons/trash-regular.svg"
+  export default {
+    name: 'blogCard',
+    props: ['post'],
+    components: {
+      Arrow,
+      Edit,
+      Delete,
+    },
+  
+    methods: {
+      deletePost() {
+        this.$store.dispatch("deletePost", this.post.blogID)
+      },
+  
+      editBlog() {
+        this.$router.push({name: "EditPost", params: {blogid: this.post.blogID}})
+      }
+    },
+  
+    computed:{
+      editPost(){
+        return this.$store.state.editPost; 
+      }
     }
-</script>
-
-<style lang="scss" scoped>
-footer{
-    margin-top: auto;
-    padding: 100px 25px;
-    background-color: #303030;
-    .container{
+   };
+  </script>
+  
+  <style lang='scss' scoped>
+  .blog-card{
+    position: relative;
+    cursor: pointer;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 8px;
+    background-color: #fff;
+    min-height: 420px;
+    transition: .5s ease all;
+    &:hover{
+      transform: rotateZ(1.3deg) scale(1.06);
+      box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6) ;
+    }
+  
+    .icons{
+      display: flex;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      z-index: 99;
+      .icon{
         display: flex;
-        gap: 12px;
-
-        .col-, .col-2, .col-3{
-            display: flex;
-            flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        background-color: #fff;
+        transition: .5s ease all;
+  
+        &:hover{
+          background-color: #303030;
+  
+          .edit,
+          .delete{
+            path{
+              fill: #fff;
+            }
+          }
         }
+        &:nth-child(1){
+          margin-right: 8px;
+        }
+  
+        .edit,
+        .delete{
+          pointer-events: none;
+          height: 15px;
+          width: auto;
+        }
+      }
     }
-}
-
-@media (max-width: 768px){
-    footer{
-        display: flex;
-        flex-direction: column
+  
+    img{
+      display: block;
+      z-index: 1;
+      border-radius: 8px 8px;
+      width: 100%;
+      min-height: 250px;
+      object-fit: contain;
     }
-}
-</style>
+  
+    .info{
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      z-index: 1;
+      padding: 32px 16px;
+      color: black;
+      h4{
+        padding-bottom: 8px;
+        font-size: 20px;
+        font-weight: 300;
+      }
+      h6{
+        font-weight: 400;
+        font-size:16px;
+        padding-bottom: 16px;
+      }
+      .link{
+        display: inline-flex;
+        align-items: center;
+        margin-top: auto;
+        font-weight: 500;
+        padding-top: 20px;
+        font-size: 12px;
+        padding-bottom: 4px;
+        transition: 0.5s ease-in all;
+        &:hover{
+          color: rgba( 48, 48, 40, 0.8) 
+        }
+        .arrow{
+          width: 10px;
+        } 
+      }
+    }
+  }
+  
+  </style>
 
