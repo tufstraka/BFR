9a510929851e588ad85122e8409593583891b89b
tufstraka <keithkadima@gmail.com>
2024-05-21 13:46:21 +0300

    update design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,182 +1,154 @@
 <template>
-    <footer>
-        <div class="container">
-            <div class="left">
-                <div class="col-1">
-                    <router-link class="header" :to="{name:'home'} ">Biased Film Reviews</router-link>
-                    <ul>
-                        <li>
-                            <a href="#"><youTube class="svg-icon"/></a>
-                        </li>
-                        <li>
-                            <a href="#"><twitter class="svg-icon"/></a>
-                        </li>
-                        <li>
-                            <a href="#"><instagram class="svg-icon"/></a>
-                        </li>
-                        <li>
-                            <a href="#"><linkedin class="svg-icon"/></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <ul>
-                        <router-link class="link" :to="{ name: 'home'}" >Home</router-link>
-                        <router-link class="link" :to="{ name: 'reviews'}" >Reviews</router-link>
-                        <router-link class="link" :to="{ name: 'home'}" >News</router-link>
-                        <router-link class="link" :to="{ name: 'home'}" >Blog</router-link>
-                    </ul>
-                </div>
-            </div>
-            <div class="right">
-               <p><span id="date">{{ this.addDate() }}</span> &#169; | All Rights Reserved</p> <br>
-                <p>Made with ❤️ by <a href='#'>Kadima</a></p> 
-            </div>
-        </div>   
-       
-    </footer>
-     
+  <footer>
+    <div class="container">
+      <div class="left">
+        <div class="col-1">
+          <router-link class="header" :to="{ name: 'home' }">Biased Film Reviews</router-link>
+          <ul class="social-links">
+            <li><a href="#"><youTube class="svg-icon" /></a></li>
+            <li><a href="#"><twitter class="svg-icon" /></a></li>
+            <li><a href="#"><instagram class="svg-icon" /></a></li>
+            <li><a href="#"><linkedin class="svg-icon" /></a></li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul class="nav-links">
+            <router-link class="link" :to="{ name: 'home' }">Home</router-link>
+            <router-link class="link" :to="{ name: 'reviews' }">Reviews</router-link>
+            <router-link class="link" :to="{ name: 'home' }">News</router-link>
+            <router-link class="link" :to="{ name: 'home' }">Blog</router-link>
+          </ul>
+        </div>
+      </div>
+      <div class="right">
+        <p><span id="date">{{ this.addDate() }}</span> &#169; | All Rights Reserved</p>
+        <p>Made with ❤️ by <a href="#">Kadima</a></p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
-    import youTube from '../assets/Icons/youtube-brands.svg';
-    import twitter from '../assets/Icons/twitter-brands.svg';
-    import instagram from '../assets/Icons/instagram-brands.svg';
-    import linkedin from '../assets/Icons/linkedin-brands.svg';
+import youTube from '../assets/Icons/youtube-brands.svg';
+import twitter from '../assets/Icons/twitter-brands.svg';
+import instagram from '../assets/Icons/instagram-brands.svg';
+import linkedin from '../assets/Icons/linkedin-brands.svg';
 
-    export default{
-        name:'footer-vue',
-        components:{
-            youTube,
-            twitter,
-            instagram,
-            linkedin
-        },
-
-        methods: {
-            addDate(){
-
-            const today = new Date();
-            const year = today.getFullYear();
-            return year;
-
-            },
-        },
-    }
+export default {
+  name: 'footer-vue',
+  components: {
+    youTube,
+    twitter,
+    instagram,
+    linkedin
+  },
+  methods: {
+    addDate() {
+      const today = new Date();
+      const year = today.getFullYear();
+      return year;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-footer{
-    margin-top: auto;
-    padding: 100px 25px;
-    background-color: #303030;
-    .container{
-        display: flex;
-        flex-direction: column;
-        gap: 12px;
-        @media(min-width: 800px){
-            flex-direction: row;
-            
-        }
-        > div{
-            display: flex;
-            flex: 1;
-        }
-        .left{
-            gap: 12px;
-            color: #fff;
-            display: flex;
-            flex-direction: column;
-            
-            align-items: center;
-            @media(min-width: 800px){
-                flex-direction: row;
-                align-items: initial;
-                
-            }
-            .header{
-                
-                font-size: 24px;
-                color: #fff;
-                text-decoration: none;
-                font-weight: 600;
-                &:hover{
-                    transition: all 2s ease;
-                    color: red;
-                }
-            }
-            ul{
-                gap: 16px;
-                list-style: none;
-                display: flex;
-                
-            }
-            .col-1,
-            .col-2{
-                gap: 22px;
-                display: flex;
-                flex: 1;
-            }
-            .col-1{
-                
-                flex-direction: column;
-                ul{
-                    margin-top:auto;
-                    gap: 50px;
-                    li{
-                        display: flex;
-                        align-items:center;
-                        .svg-icon{
-                            width: 20px;                        
-                            height:auto;                            
-                            color: #fff
-                        }
-                    }
-                }
-            }
-            .col-2{
-                display: flex;
-                justify-content: center;
-                ul{
-                height: 100%;
-                justify-content: center;
-                flex-direction: row;
-                flex-wrap: wrap;
-                @media(min-width:800px){
-                   // flex-direction: column
-                }
-                .link{
-                    font-size: 16px;
-                    font-weight: 500;
-                    text-decoration: none;
-                    color: #fff;
-                }
-                }
-            }
-           
-        }
+footer {
+  padding: 40px 20px;
+  background-color: #1a1a1a;
+  color: #fff;
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      justify-content: space-between;
+      text-align: left;
     }
-     .right{
-                gap: 2px;
-                align-items: center;
-                flex-direction: column;
-                text-align: center;
-                margin-bottom: 65px;
-                
-                @media(min-width:800px){
-                    //align-items: flex-end;
-                    gap:0;
-                }
-                p{
-                    margin-top: auto;
-                    color: #fff
-                     
-                }
-                a{
-                    text-decoration: none;
-                    color: blue;
-                }
+  }
+  .left {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+    .col-1 {
+      margin-bottom: 20px;
+      @media (min-width: 800px) {
+        margin-bottom: 0;
+        margin-right: 40px;
+      }
+      .header {
+        font-size: 1.5rem;
+        color: #fff;
+        text-decoration: none;
+        font-weight: bold;
+        margin-bottom: 16px;
+        &:hover {
+          color: #f39c12;
+        }
+      }
+      .social-links {
+        display: flex;
+        gap: 16px;
+        list-style: none;
+        li {
+          .svg-icon {
+            width: 24px;
+            height: 24px;
+            fill: #fff;
+            transition: fill 0.3s;
+            &:hover {
+              fill: #f39c12;
             }
+          }
+        }
+      }
+    }
+    .col-2 {
+      .nav-links {
+        display: flex;
+        gap: 20px;
+        list-style: none;
+        flex-wrap: wrap;
+        justify-content: center;
+        @media (min-width: 800px) {
+          justify-content: flex-start;
+        }
+        .link {
+          font-size: 1rem;
+          text-decoration: none;
+          color: #fff;
+          &:hover {
+            color: #f39c12;
+          }
+        }
+      }
+    }
+  }
+  .right {
+    margin-top: 20px;
+    @media (min-width: 800px) {
+      margin-top: 0;
+    }
+    p {
+      margin: 0;
+      color: #bbb;
+      a {
+        color: #f39c12;
+        text-decoration: none;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
 }
 </style>
 
