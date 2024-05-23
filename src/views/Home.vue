307caad6cb50cbdc68380765d6c6c040a4ee6fe3
tufstraka <keithkadima@gmail.com>
2024-05-23 11:52:16 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="home">
-    <BlogPost v-if="!user" :post='welcomeScreen'/>
-    <div v-if="user">
-        <TrendingMovies />
-    </div>
+    <BlogPost v-if="!user" :post="welcomeScreen" />
     
     <div v-if="user">
-      <BlogPost :post='post' v-for="(post, index) in blogPostsFeed" :key="index" />
-    </div>
-    
-    <div  v-if="user" class="blog-card-wrap">
-        
-        <h1 text-align='center' >Recent Reviews</h1>
-        
-        <div class = 'blog-cards'>
-          
-          <BlogCards :post='post' v-for="(post, index) in blogPostsCards" :key="index" />
+      <TrendingMovies />
+      <BlogPost :post="post" v-for="(post, index) in blogPostsFeed" :key="index" />
+      
+      <div class="blog-card-wrap">
+        <h1 style="text-align:center;">Recent Reviews</h1>
+        <div class="blog-cards">
+          <BlogCards :post="post" v-for="(post, index) in blogPostsCards" :key="index" />
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
 
@@ -28,96 +20,92 @@
 import BlogPost from '../components/BlogPost.vue';
 import BlogCards from '../components/BlogCard.vue';
 import TrendingMovies from '../components/TrendingMovies.vue';
-/*import ArticleGrid from '../components/ArticleGrid.vue';*/
 
 export default {
   name: "Home",
-  components: { BlogPost , BlogCards , TrendingMovies },
-  data(){
+  components: { BlogPost, BlogCards, TrendingMovies },
+  data() {
     return {
-      welcomeScreen:{
-        title:"Niaje",
-        blogPost:"Do you like watching movies and Tv shows? Do you always feel like you want to rant or just express your opinions? Welcome to BFR.",
+      welcomeScreen: {
+        title: "Niaje",
+        blogPost: "Do you like watching movies and TV shows? Do you always feel like you want to rant or just express your opinions? Welcome to BFR.",
         welcomeScreen: true,
-        photo:"movie1"
-      },
-      
- 
+        photo: "movie1"
+      }
     };
   },
-  computed: { 
-    blogPostsCards(){
+  computed: {
+    blogPostsCards() {
       return this.$store.getters.blogPostsCards;
     },
-
-    blogPostsFeed(){
+    blogPostsFeed() {
       return this.$store.getters.blogPostsFeed;
     },
-
-
     user() {
       return this.$store.state.user;
-    },
-  },
-  
-};   
-  
+    }
+  }
+};
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .home {
-background: beige;
+  background: beige;
 }
-.vue{
+
+.vue {
   position: absolute;
-  top:0;
-  left:0;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
 }
 
-.blog-card-wrap{
+.blog-card-wrap {
   background: linear-gradient(to bottom left, #111111, #333333);
-  h1{
+  
+  h1 {
     font-weight: 300;
     font-size: 21px;
     margin-bottom: 12px;
     color: white;
-    
   }
 }
-.updates{
-  .container{
+
+.updates {
+  .container {
     padding: 100px 25px;
     display: flex;
     flex-direction: column;
     align-items: center;
-   
-    @media(min-width: 800px){
+
+    @media (min-width: 800px) {
       padding: 125px 25px;
       flex-direction: row;
     }
-    .router-button{
+
+    .router-button {
       display: flex;
       font-size: 14px;
       text-decoration: none;
-      @media(min-width: 800px){
-        margin-left: auto
+
+      @media (min-width: 800px) {
+        margin-left: auto;
       }
     }
-    h2{
+
+    h2 {
       font-weight: 300;
       font-size: 32px;
       max-width: 725px;
       width: 100%;
       text-align: center;
       text-transform: uppercase;
-      @media(min-width: 800px){
-        //text-align: initial;
+
+      @media (min-width: 800px) {
         font-size: 40px;
       }
     }
   }
 }
-
 </style>
