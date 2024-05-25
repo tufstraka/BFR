ea4d6b0585e83a398f4758fbbb1dd7ace72813d1
tufstraka <keithkadima@gmail.com>
2024-05-25 12:18:41 +0000

    auth

--- src/router/index.js
+++ src/router/index.js
@@ -11,96 +11,104 @@ import CreatePost from "../views/CreatePost.vue";
 import BlogPreview from "../views/BlogPreview.vue";
 import ViewBlog from "../views/ViewBlog.vue";
 import EditPost from "../views/EditPost.vue"
+import firebase from "firebase/app";
+import "firebase/auth";
 
 Vue.use(VueRouter);
+
+const auth = firebase.auth();
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
-    meta:{
-      title:'Home'
+    meta: {
+      title: 'Home'
     }
   },
   {
     path: "/Film-Reviews",
     name: "Blogs",
     component: Blog,
-    meta:{
-      title:'Reviews'
+    meta: {
+      title: 'Reviews'
     }
   },
   {
     path: "/Login",
     name: "Login",
     component: Login,
-    meta:{
-      title:'Login'
+    meta: {
+      title: 'Login'
     }
   },
   {
     path: "/Register",
     name: "Register",
     component: Register,
-    meta:{
-      title:'Register'
+    meta: {
+      title: 'Register'
     }
   },
   {
     path: "/Forgot-Password ",
     name: "ForgotPassword",
     component: ForgotPassword,
-    meta:{
-      title:'Forgot Password'
+    meta: {
+      title: 'Forgot Password'
     }
   },
   {
     path: "/Profile",
     name: "Profile",
     component: Profile,
-    meta:{
-      title:'Profile'
+    meta: {
+      title: 'Profile',
+      requiresAuth: true
     }
   },
   {
     path: "/create-post",
     name: "CreatePost",
     component: CreatePost,
-    meta:{
-      title:'Create Post'
+    meta: {
+      title: 'Create Post',
+      requiresAuth: true
     }
   },
   {
     path: "/post-preview",
     name: "BlogPreview",
     component: BlogPreview,
-    meta:{
-      title:'Preview Blog  Post'
+    meta: {
+      title: 'Preview Blog  Post'
     }
   },
   {
     path: "/view-blog/:blogid",
     name: "ViewBlog",
     component: ViewBlog,
-    meta:{
-      title:'View Blog Post'
+    meta: {
+      title: 'View Blog Post'
     }
   },
   {
     path: "/edit-post/:blogid",
     name: "EditPost",
     component: EditPost,
-    meta:{
-      title:'Edit Blog Post'
+    meta: {
+      title: 'Edit Blog Post',
+      requiresAuth: true
     }
   },
   {
     path: "/Admin",
     name: "Admin",
     component: Admin,
-    meta:{
-      title:'Admin'
+    meta: {
+      title: 'Admin',
+      requiresAuth: true
     }
   },
 ];
@@ -111,9 +119,17 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) =>{
+router.beforeEach((to, from, next) => {
   document.title = `${to.meta.title} | BFR`;
-  next()
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = auth.currentUser;
+
+  if (requiresAuth && !isAuthenticated) {
+    next("/Login");
+  } else {
+    next();
+  }
+
 })
 
 export default router;
