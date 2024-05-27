ab3b54b928310749846aabab98478b7d5463bbbe
tufstraka <keithkadima@gmail.com>
2024-05-27 09:44:20 +0000

    color update

--- src/components/TrendingMovies.vue
+++ src/components/TrendingMovies.vue
@@ -132,7 +132,7 @@
     flex-direction: column;
     width: 150px;
     gap: 20px;
-    background: beige;
+    background: #d4dfc9;
     border-radius: 12px;
     transition: transform 350ms;
     margin-left: 30px;
