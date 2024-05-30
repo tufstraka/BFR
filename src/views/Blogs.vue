bf6990aeccb42cf344817d2ac0202194ff8cbf21
tufstraka <keithkadima@gmail.com>
2024-05-30 13:51:30 +0300

    Update Blogs.vue

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,97 +1,124 @@
 <template>
-  <div class='blog-card-wrap'>
-      <div class="blog-cards container">
-          <div v-if="admin" class="toggle-edit">
-              <span>Toggle Editing Post</span> <br>
-              <input type="checkbox" v-model='editPost'>
-              
-          </div>
-          <BlogCards :post='post' v-for="(post, index) in blogPosts" :key="index" />
-      </div>
-  </div>
+    <div class='blog-card-wrap'>
+        <div class="blog-cards container">
+            <div v-if="admin" class="toggle-edit">
+                <span>Toggle Editing Post</span> <br>
+                <label class="switch">
+                    <input type="checkbox" v-model='editPost'>
+                    <span class="slider"></span>
+                </label>
+            </div>
+            <BlogCards :post='post' v-for="(post, index) in blogPosts" :key="index" />
+        </div>
+    </div>
 </template>
 
 <script>
 import BlogCards from '../components/BlogCard.vue';
 export default {
-    name:'Blogs',
-    components: {BlogCards},
+    name: 'Blogs',
+    components: { BlogCards },
     computed: {
- 
-    admin() {
+        admin() {
             return this.$store.state.profileAdmin;
         },
-    
-    blogPosts(){
-      return this.$store.state.blogPosts;
-    },
-    
-    editPost:{
-        get(){
-            return this.$store.state.editPost
+        blogPosts() {
+            return this.$store.state.blogPosts;
         },
-        set(payload){
-            this.$store.commit('toggleEditPost' , payload)
+        editPost: {
+            get() {
+                return this.$store.state.editPost;
+            },
+            set(payload) {
+                this.$store.commit('toggleEditPost', payload);
+            },
         },
     },
-  },
-  beforeDestroy(){
-      this.$store.commit("toggleEditPost" , false)
-  } 
-
+    beforeDestroy() {
+        this.$store.commit("toggleEditPost", false);
+    }
 }
 </script>
 
 <style lang='scss' scoped>
-
-.blog-cards{
+.blog-cards {
     position: relative;
-    .toggle-edit{
+    z-index: 1;
+  
+    .toggle-edit {
         display: flex;
         align-items: center;
         position: absolute;
         top: -70px;
-        right:0;
+        right: 0;
 
-        span{
+        span {
             margin-right: 16px;
         }
-
- 
     }
 }
 
-input[type="checkbox"]{
+.switch {
     position: relative;
-    border: none;
-    background: #fff;
-    outline: none;
+    display: inline-block;
     width: 80px;
-    height: 24px;
-    border-radius: 20px;
-    box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6);
+    /* Adjust the width */
+    height: 40px;
+    /* Adjust the height */
 }
 
-input[type="checkbox"]:before{
-    content:'';
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.slider {
     position: absolute;
-    background: #fff;
-    outline: none;
-    width: 30px;
-    height: 16px;
+    cursor: pointer;
+    top: 2px;
+    /* Adjusted for better alignment */
+    left: 2px;
+    /* Adjusted for better alignment */
+    bottom: 2px;
+    /* Adjusted for better alignment */
+    right: 2px;
+    /* Adjusted for better alignment */
+    background-color: #ccc;
+    transition: 0.4s;
     border-radius: 20px;
-    top:3px;
-    left: 3px;
-    transform: scale(1.1);
-    transition: 750ms ease all;
-    box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6);
+    /* Adjust the border-radius */
 }
 
-input:checked[type="checkbox"]:before{
-    background: #fff;
-    left: 47px;
-    background-color:red;
+.slider:before {
+    position: absolute;
+    content: "";
+    height: 36px;
+    /* Adjust the height of the knob */
+    width: 36px;
+    /* Adjust the width of the knob */
+    bottom: 2px;
+    /* Adjust the position of the knob */
+    background-color: white;
+    transition: 0.4s;
+    border-radius: 50%;
+    /* Make the knob circular */
 }
 
+input:checked+.slider:before {
+    transform: translateX(38px);
+    /* Adjust the movement distance */
+}
 
+input:checked+.slider {
+    background-color: #4caf50;
+}
+
+.slider.round {
+    border-radius: 20px;
+}
+
+.slider.round:before {
+    border-radius: 50%;
+}
 </style>
